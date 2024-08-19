150e1ad0e43dbd08be70accd6bdece3ffa21b0e9
Sr1jan27 <80779925+Sr1jan27@users.noreply.github.com>
2024-08-19 19:51:06 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,10 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
 // first comment
+
+// date 19-08-2024 POTD GFG
+//  int findkthsmallestnumber(int[] arr, int k){
+
+    Arrays.sort(arr);
+    return arr[k-1];
+}
